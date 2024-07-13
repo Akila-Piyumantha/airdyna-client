3bf34c82ddeecfb9dc5ca394c993b07140aaaedb
Akila Piumantha <156569575+Akila-Piyumantha@users.noreply.github.com>
2024-07-13 17:35:28 +0530

    Update products.js

--- src/Products/products.js
+++ src/Products/products.js
@@ -1,17 +1,18 @@
 import './pstyles.css';
+import React from 'react';
 
 const Procard = ({ workout }) => {
-    return(
-        <div className="card-grid " style={{ paddingTop: '90px' }}>
+    return (
+        <div className="card-grid" style={{ paddingTop: '90px' }}>
             <a className="card" href="/add">
-                <div className="card__background" style={{backgroundImage: `url(${workout.imageUrl})`}}></div>
+                <div className="card__background" style={{ backgroundImage: `url(${workout.imageUrl})` }}></div>
                 <div className="card__content">
-                    <p className="card__category" key={workout._id}>{workout.title}</p>
-                    <h3 className="card__heading" key={workout._id}>{workout.category}</h3>
+                    <p className="card__category">{workout.title}</p>
+                    <h3 className="card__heading">{workout.category}</h3>
                 </div>
             </a>
         </div>
-    )
+    );
 }
 
 export default Procard;
