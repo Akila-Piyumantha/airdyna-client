14525fad940b35d6484a2d0678727cb30605480b
Akila Piumantha <156569575+Akila-Piyumantha@users.noreply.github.com>
2024-07-14 13:37:41 +0530

    Update productdtails.js

--- src/Products/productdtails.js
+++ src/Products/productdtails.js
@@ -7,29 +7,20 @@ const Pro = () => {
     const [workouts, setWorkouts] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
-    const [error, setError] = useState(null);
-    const apiUrl = process.env.REACT_APP_API_URL;
 
     useEffect(() => {
         const fetchProducts = async (page) => {
-            try {
-                const response = await fetch(`${apiUrl}/api/workouts?page=${page}&limit=12`);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
+            const response = await fetch(`/api/workouts?page=${page}&limit=12`);
+            const data = await response.json();
 
-                const data = await response.json();
-                console.log(data);  // Log the received data
+            if (response.ok) {
                 setWorkouts(data.workouts);
                 setCurrentPage(data.page);
                 setTotalPages(data.totalPages);
-            } catch (err) {
-                console.error('Failed to fetch workouts:', err);
-                setError(err.message);
             }
         };
         fetchProducts(currentPage);
-    }, [currentPage, apiUrl]);
+    }, [currentPage]);
 
     const handlePageChange = (page) => {
         setCurrentPage(page);
@@ -37,22 +28,16 @@ const Pro = () => {
 
     return (
         <div className='bodyback'>
-            {error ? (
-                <div className="error-message">Error: {error}</div>
-            ) : (
-                <>
-                    <section className="hero-section bodym bodyback">
-                        {workouts.map(workout => (
-                            <Procard key={workout._id} workout={workout} />
-                        ))}
-                    </section>
-                    <Pagination
-                        totalPages={totalPages}
-                        currentPage={currentPage}
-                        onPageChange={handlePageChange}
-                    />
-                </>
-            )}
+            <section className="hero-section bodym bodyback">
+                {workouts.map(workout => (
+                    <Procard key={workout._id} workout={workout} />
+                ))}
+            </section>
+            <Pagination
+                totalPages={totalPages}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+            />
         </div>
     );
 };
